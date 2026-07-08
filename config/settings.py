c6f67087a24a03ca1cc15e14ea9e8165488f35fe
ayami-okada <ayami.okada.dev@gmail.com>
2026-07-08 19:53:55 +0900

    4回目

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,11 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
+    # cloudinary
+    
+    'cloudinary',
     'base'
 ]
 
